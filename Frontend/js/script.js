3977eac69cbd0e75304aad15339854b1d771e5ec
watashisham <wata5hi@proton.me>
2024-09-25 20:17:05 -0300

    backendfinalizado

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,4 +1,4 @@
-const { json } = require("stream/consumers")
+//const { json } = require("stream/consumers")
 
 //Login Elements
 const login = document.querySelector(".login")
@@ -9,6 +9,7 @@ const loginInput = login.querySelector(".login__input")
 const chat = document.querySelector(".chat")
 const chatForm = chat.querySelector(".chat__form")
 const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages")
 
 //Cores dos nic
 const colors = [
@@ -23,14 +24,72 @@ const user = {id: "", name: "", color: ""}
 
 let websocket
 
+function createMessageSelfElement(content, sender, senderColor) {
+    const div = document.createElement("div")
+    const span = document.createElement("span")
+
+    // Adicionar a classe e estilo da mensagem própria (self)
+    div.classList.add("message--self")
+    span.classList.add("message--sender")
+    
+
+    // Nome do usuário self
+    span.innerHTML = sender
+    span.style.color = senderColor
+
+    // Adicionar o remetente e a mensagem ao div
+    div.appendChild(span)
+
+     // Adicionar o conteúdo da mensagem
+    const messageContent = document.createElement("div")
+    messageContent.classList.add("message--content")
+    messageContent.innerHTML = content
+
+    div.appendChild(messageContent)
+
+    return div
+}
+
+const createMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement("div")
+    const span = document.createElement("span")
+
+    div.classList.add("message--other")
+
+    div.classList.add("message--self")
+    span.classList.add("message--sender")
+    span.style.color = senderColor
+
+    div.appendChild(span)
+
+    span.innerHTML = sender
+    div.innerHTML += content
+
+    return div
+}
+
 const getRandomColor = () =>{
     const randomIndex = Math.floor(Math.random() * colors.length)
     return colors[randomIndex]
 }
 
+const scrollScreen = () => {
+    window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth"
+    })
+}
+
 const processMessage = ({ data }) => {
-   // const { userId, userName, userColor, content } = JSON.parse(data)
-   console.log(JSON.parse(data))
+    const { userId, userName, userColor, content } = JSON.parse(data)
+    
+    // Verifica se a mensagem é do próprio usuário ou de outro
+    const message = userId == user.id 
+    ? createMessageSelfElement(content, user.name, user.color) 
+    : createMessageOtherElement(content, userName, userColor)
+
+    chatMessages.appendChild(message)
+    scrollScreen()
 }
 
 const handleLogin = (event) =>{
